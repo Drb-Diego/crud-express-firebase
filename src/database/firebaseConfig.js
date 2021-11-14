fb7dd53df1e44d7e148e0ade6df1612fe7851ad0
Diego Brito <diegorbrito9@gmail.com>
2021-11-14 11:30:24 -0300

    Criando uma variavel de ambiente para configuraçao do firebase

--- src/database/firebaseConfig.js
+++ src/database/firebaseConfig.js
@@ -1,13 +1,17 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export const firebaseConfig = {
-  "type": "service_account",
-  "project_id": "api-rest-firestore-e1a1a",
-  "private_key_id": "56c4243d4da30687fafaddd7912797bb491efd02",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCv6Cym+mXwmtGL\ng5sn5Wjdk01ev2JWmGp0rI4xm8Z7mA7BInkLtLdg/owT6vWksEY6Xv1dJsp/M009\ndzC+EfchlWJyUHrMA5US6I6BRO5OO/1DKa6l6sAmHlh9CEV+bOU9KKAG2hc6afVX\ntI0JbIfUQwtOT5f+pUzT/Hl5Rf72WP4uDa7Ibd/pNCwS/go0jlsor8DCQXf2hJce\nj6FqR20EWPy9b6CHkJTUrFKduXol9nJSFTS6fksmpImhHaB/cya5jYOw1XsaxMf4\nKo5BiVfq0l652u1+CLkja56C5mRxvMvoT23hVqc4tp4FhGUSfWmd72QT00iOKjrP\n+N5PrrwrAgMBAAECggEAB1P36ZGHMKGfzBzp7Zy4JHmP9hFT/UExN3YUIGCZwVQB\nfViRzAPgcHkk3b7ih9SowE1tEaCIPAn86ZbEYDlVtGpJj8iRglvogxakD8zkKTyE\n3Z49aSphNyqkILqjpn4j2KCPdhRHdnFWkqucr9amcUMaaU3rNOUEUSp0kbGcSv0y\nuZ5IHCjZdXDjzDPLaVkdzIKtV7NepZrTrs6hmwSBd9p4b+ZCNHbJYSZhktDWXPYB\nHuSiLcGC9DOTOm9xrqiCnt8qyR52IQuEI9DQL9upwWt+W2ZwZcMoVOcuHiH8b9sa\nuZHGsOaK6kVo9Bj9nZwHF2XZurb4r9/aZIMV6anfEQKBgQDfGJ+x9unEFcfzXf+0\nspcBhqMW7bwHOaqihPYkRRfLll/Zo8HKtT09HQYgs7tMu799P3Dbwjw9IMS4Q87a\nU87u2C/WMbBasEOGIgfe+Pk+w2/DanURZukbqYXDwGSIPKelXCB4BYWIRM+CNlTy\nxDxOohP27LOxy6yfw2nMYJz1mwKBgQDJ2dcIQ3ZsWDoUoRcv8Mff4Uw0VfJpgR/I\n+FZkGHBcziMeGJEV9/1PcMieKldVTOzDoskA9slrVSLWMUiTDso++IfbsnGaDA1X\n9zVcGI1yTZJq563kUq0V2hYUhbSE6MiVBTCbr1ltxBEQYnewksTgVuUWjX+RnfLu\nI7CtUF+EsQKBgAWd5xdh8gy/x6L28OMpw+eD7kM7hQphh0xEMLZFDw5PIOan2s8x\ntPL+8hKMs+CAsZeDq2G9CCs50foZqvGQPpuA0eOAqMH/whN0c+JHPm84GG5BFeV0\nQ9YwdG+zPlS3wjjFkwOyj+QYZ2kN2SGrkyEfr4T9/KAzUotLLjS5EfqVAoGAUdDe\nfBb8NUY9pIb3IxNxsKldfgkoTONtwsQGIGRXhwZ27Yf/puvx3PVWG+YFqCmZ/mJJ\nHv7o/XxwtmkbhaAFEikiyEY4Dby8297z3fSFwML4iiSGM3nfdnADayYW2ZiRVPXZ\nkgEtOnSyhfWjlgqnilcuXEnWX2uqQCRvRJDDZsECgYAW5noQ8WDLlvP+alc7driP\nkTNkVTfhnjAxDhj/lm6ugenU0zP631GUdNbWfYmKt1AU10MRn6B+guUWbLbZAvFC\n4i+UnHg+6kpTp4AL7tWRZU6TdnJH7yIcT9Co/aC6St6HmTr8im1GOWlbcdzuNOFt\noJ+reLekJaxuMiSqkPa2QQ==\n-----END PRIVATE KEY-----\n",
-  "client_email": "firebase-adminsdk-nx0f4@api-rest-firestore-e1a1a.iam.gserviceaccount.com",
-  "client_id": "117464848395668209310",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nx0f4%40api-rest-firestore-e1a1a.iam.gserviceaccount.com"
+  "type": process.env.TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY,
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
 }
 
