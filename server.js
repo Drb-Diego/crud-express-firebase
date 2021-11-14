da83cd30773e8f65f8011e22a2b168cd3a3b3f8f
Diego Brito <diegorbrito9@gmail.com>
2021-11-14 11:57:55 -0300

    adicionando a porta da variavel de ambiente

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ import expressConfig from "./src/config/expressConfig.js";
 
 const app = expressConfig();
 
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => console.log(`Server up\nNa porta: ${PORT}`));
