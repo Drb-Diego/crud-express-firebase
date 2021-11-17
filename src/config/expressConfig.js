4f6b87150587a482f7ef81bcfcdd3b683ecb90b0
Diego Brito <diegorbrito9@gmail.com>
2021-11-17 19:23:40 -0300

    resolvendo o problema do cors

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,12 +1,14 @@
 import express from "express";
 import homeRoutes from "../routes/homeRoute.js";
 import { startFirebase } from "../database/startFirebase.js";
+import cors from "cors";
 
 export default () => {
   const app = express();
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  app.use(cors());
   app.use(homeRoutes);
 
   startFirebase();
